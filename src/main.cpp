a54b2bebf562b34e33c13b507e1cfa51afd458d5
mannie.exe <root@unknown.energy>
2020-07-28 15:53:29 -0700

    Include Glad in build system and create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,52 @@
 
 #include <iostream>
-#include <GLFW/glfw3.h>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
 int main(int argc, char **args)
 {
-  cout << "Hello, world!" << endl;
+  cout << "Guess I'm learning OpenGL now.\n";
 
   glfwInit();
-  glfwTerminate();
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+    cout << "FAILED to create GLFW window" << endl;
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    cout << "FAILED to initialize GLAD\n";
+    return -1;
+  }
+
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
 }
