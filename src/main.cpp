0a3a3896d0258a96b3e53dc3105578918566f45d
mannie.exe <root@unknown.energy>
2020-07-28 14:12:16 -0700

    Implement a basic build system
    CMake now compiles both GLFW and my
    """program""
    from scratch.
    
    Try `cmake -B build && cmake --build build`

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+using namespace std;
+
+int main(int argc, char **args)
+{
+  cout << "Hello, world!" << endl;
+
+  glfwInit();
+  glfwTerminate();
+
+  return 0;
+}
